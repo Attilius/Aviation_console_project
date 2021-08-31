549b5bdc920c1a8c2e2ae8133fc0d075869ee555
Attilius <leo20@freemail.hu>
2021-08-31 13:20:49 +0200

    I added construct and methods of Time class

---
+++ Aviation_project/Aviation_project/Time.cpp
@@ -0,0 +1,55 @@
+#include "Time.h"
+
+Time::Time(string time) {
+    stringstream test(time);
+    string segment;
+    vector<string> segmentList;
+
+    while (getline(test, segment, ':'))
+    {
+        segmentList.push_back(segment);
+    }
+    int hour = stoi(segmentList[0]);
+    int minute = stoi(segmentList[1]);
+
+    this->Hour = hour;
+    this->Minute = minute;
+}
+
+int Time::getHour() {
+    return this->Hour;
+}
+
+int Time::getMinute() {
+    return this->Minute;
+}
+
+string Time::getLondonTime(int Hour, int Minute) {
+    string hour = to_string(Hour - 1);
+    string minute = to_string(Minute);
+    if ((Hour - 1) < 10)
+    {
+        hour = "0" + to_string(Hour - 1);
+    }
+    if (Minute < 10)
+    {
+        minute = "0" + to_string(Minute);
+    }
+
+    return hour + ":" + minute;
+}
+
+string Time::getNewYorkTime(int Hour, int Minute) {
+    string hour = to_string(Hour - 6);
+    string minute = to_string(Minute);
+    if ((Hour - 6) < 10)
+    {
+        hour = "0" + to_string(Hour - 6);
+    }
+    if (Minute < 10)
+    {
+        minute = "0" + to_string(Minute);
+    }
+
+    return hour + ":" + minute;
+}
