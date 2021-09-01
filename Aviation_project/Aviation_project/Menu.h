4d9c21541e111dd95a402e2b74639f09d20dac49
Attilius <leo20@freemail.hu>
2021-09-01 18:02:01 +0200

    I set dependecies for menu, and added a function skeleton

--- Aviation_project/Aviation_project/Menu.h
+++ Aviation_project/Aviation_project/Menu.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::setw;
+using std::left;
+using std::right;
+using std::endl;
+
+
+void setLocation();
