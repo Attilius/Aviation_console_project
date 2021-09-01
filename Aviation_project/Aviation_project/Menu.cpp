85023ee36798f7284e25a4fa1121b6ae8c45ba58
Attilius <leo20@freemail.hu>
2021-09-01 18:04:12 +0200

    I added the body for function of Menu.

---
+++ Aviation_project/Aviation_project/Menu.cpp
@@ -0,0 +1,18 @@
+#include "Menu.h"
+
+void setLocation() {
+    cout << " " << endl;
+    cout << "|||||||||||||||||||||||||||||" << endl;
+    cout << "|      Select location      |" << endl;
+    cout << "|||||||||||||||||||||||||||||" << endl;
+    cout << " " << endl;
+    cout << "=> 1. Budapest" << endl;
+    cout << "=> 2. London" << endl;
+    cout << "=> 3. Paris" << endl;
+    cout << "=> 4. Munich" << endl;
+    cout << "=> 5. New_York" << endl;
+    cout << "=> 6. Quit" << endl;
+    cout << " " << endl;
+    cout << "Selected (1-6): ";
+
+}
