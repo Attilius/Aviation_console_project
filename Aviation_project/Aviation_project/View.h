b642c810827c9ab4c5b8d3b5f7d7237a4db03155
Attilius <leo20@freemail.hu>
2021-09-14 21:20:30 +0200

    I added special namespaces with class View and his method

---
+++ Aviation_project/Aviation_project/View.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Error.h"
+
+using namespace std;
+
+namespace error
+{
+	class View
+	{
+	public:
+		void showErrorMessage(int errorCode, string errorMessage);
+	};
+}
+namespace Alert
+{
+	class View
+	{
+	public:
+		void showAlertMessage(string alertMessage);
+	};
+}
+namespace Confirmation
+{
+	class View
+	{
+	public:
+		void confirmationMessage(string confirmationMsg);
+	};
+}
