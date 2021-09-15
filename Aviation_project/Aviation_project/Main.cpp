11b94a65854df057e0907ed4e8a65cbdfd48a0a1
Attilius <leo20@freemail.hu>
2021-09-15 10:57:30 +0200

    Namespaces moved to Cities.cpp

--- Aviation_project/Aviation_project/Main.cpp
+++ Aviation_project/Aviation_project/Main.cpp
@@ -8,31 +8,6 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-namespace Budapest {
-	vector <FlightInformations> flightArrivals;
-	vector <FlightInformations> flightDepartures;
-}
-
-namespace London {
-	vector <FlightInformations> flightArrivals;
-	vector <FlightInformations> flightDepartures;
-}
-
-namespace Paris {
-	vector <FlightInformations> flightArrivals;
-	vector <FlightInformations> flightDepartures;
-}
-
-namespace Munich {
-	vector <FlightInformations> flightArrivals;
-	vector <FlightInformations> flightDepartures;
-}
-
-namespace New_York {
-	vector <FlightInformations> flightArrivals;
-	vector <FlightInformations> flightDepartures;
-}
-
 int main()
 {
 	string location = " ";
