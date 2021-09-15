e9553d9b4f27972077bdbc21b6513a5f302fc491
Attilius <leo20@freemail.hu>
2021-09-15 11:00:45 +0200

    Namespaces are declared in header

---
+++ Aviation_project/Aviation_project/Cities.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include "FlightInformations.h"
+
+using namespace std;
+
+namespace Budapest {}
+
+namespace London {}
+
+namespace Paris {}
+
+namespace Munich {}
+
+namespace New_York {}
+
+
