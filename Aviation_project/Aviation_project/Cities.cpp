af04931f3195b81b52f1f5f4825e0b4541159242
Attilius <leo20@freemail.hu>
2021-09-15 11:02:40 +0200

    Vectors are inserted in to namespaces

---
+++ Aviation_project/Aviation_project/Cities.cpp
@@ -0,0 +1,26 @@
+#include "Cities.h"
+
+namespace Budapest {
+	vector <FlightInformations> flightArrivals;
+	vector <FlightInformations> flightDepartures;
+}
+
+namespace London {
+	vector <FlightInformations> flightArrivals;
+	vector <FlightInformations> flightDepartures;
+}
+
+namespace Paris {
+	vector <FlightInformations> flightArrivals;
+	vector <FlightInformations> flightDepartures;
+}
+
+namespace Munich {
+	vector <FlightInformations> flightArrivals;
+	vector <FlightInformations> flightDepartures;
+}
+
+namespace New_York {
+	vector <FlightInformations> flightArrivals;
+	vector <FlightInformations> flightDepartures;
+}
