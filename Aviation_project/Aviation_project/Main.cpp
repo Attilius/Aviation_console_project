4bb084a6266ef7a03b6a9e57607aa826b5cdd977
Attilius <leo20@freemail.hu>
2021-09-16 21:24:16 +0200

    I set entry point of program

--- Aviation_project/Aviation_project/Main.cpp
+++ Aviation_project/Aviation_project/Main.cpp
@@ -1,64 +1,7 @@
-#include <iostream>
-#include "Flight.h"
-#include "Time.h"
-#include "View.h"
+#include "Controller.h"
 
-using std::string;
-using std::cin;
-using std::cout;
-using std::endl;
-
-int main()
+void main()
 {
-	string location = " ";
-	int selected = 0;
-
-	do
-	{
-		system("cls");
-		//setLocation();
-		cin >> selected;
-
-		switch (selected)
-		{
-		case 1:
-			system("cls");
-			location = "Budapest";
-			cout << location << endl;
-			system("pause");
-			break;
-		case 2:
-			system("cls");
-			location = "London";
-			cout << location << endl;
-			system("pause");
-			break;
-		case 3:
-			system("cls");
-			location = "Paris";
-			cout << location << endl;
-			system("pause");
-			break;
-		case 4:
-			system("cls");
-			location = "Munich";
-			cout << location << endl;
-			system("pause");
-			break;
-		case 5:
-			system("cls");
-			location = "New_York";
-			cout << location << endl;
-			system("pause");
-			break;
-		case 6:
-			return EXIT_SUCCESS;
-			break;
-		default:
-			break;
-		}
-	} while (selected != 6);
-
-    system("pause>0");
-    return EXIT_SUCCESS;
+	local::Controller localController;
+	localController.LocalMenuController();
 }
