9daf5aed008fed06444b7dac9a1943aebb652e1a
Attilius <leo20@freemail.hu>
2021-09-16 15:11:36 +0200

    I declared an integer property and two method for header of Location class

---
+++ Aviation_project/Aviation_project/Locations.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Locations
+{
+private:
+	int numberOfLocation;
+
+public:
+	string* getCities();
+
+	int getNumberOfLocations();
+};
