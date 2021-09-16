38f82410a028f403287dda173ab9472cbbcffcc6
Attilius <leo20@freemail.hu>
2021-09-16 16:25:18 +0200

    I set name of namespaces and I defined two method of Controller class

--- Aviation_project/Aviation_project/Controller.cpp
+++ Aviation_project/Aviation_project/Controller.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
+#include "Locations.h"
 #include "Controller.h"
 #include "View.h"
+#include "Menu.h"
 
-void Local::Controller::LocalMenuController()
+void local::Controller::LocalMenuController()
 {
+	location::View loc;
+	main::Controller menuController;
+	Locations lc;
+
+	string* cities = lc.getCities();
+	int sizeOfLocal = lc.getNumberOfLocations();
+	int localSelected;
+
+	do
+	{
+		system("cls");
+		loc.setLocation();
+		cin >> localSelected;
+		system("cls");
+		loc.showLocation(lc.getCities()[localSelected - 1]);
+		menuController.MainMenuContoller();
+		
+	} while (localSelected < 1 || localSelected > sizeOfLocal);
+
 }
 
-void Main::Controller::MainMenuContoller()
+void main::Controller::MainMenuContoller()
 {
+	menu::View menu;
+	Menu menus;
+
+	int sizeOfMenu = menus.getNumberOfMenuItems();
+	int menuSelected;
+
+	do
+	{
+		menu.setMenu();
+		cin >> menuSelected;
+		if (menuSelected == 7) EXIT_SUCCESS;
+
+	} while (menuSelected < 1 || menuSelected > sizeOfMenu);
 }
