7569c6f615d6208c3146767437930091520a34fd
Attilius <leo20@freemail.hu>
2021-09-16 11:13:45 +0200

    I added two namespace and Controller class and his method in to namespaces

---
+++ Aviation_project/Aviation_project/Controller.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace Local
+{
+	class Controller
+	{
+	public:
+		void LocalMenuController();
+	};
+}
+
+namespace Main
+{
+	class Controller
+	{
+	public:
+		void MainMenuContoller();
+	};
+}
+
+
