c2b531152dfa300e16a7b0ddd69eb98663a624bc
Attilius <leo20@freemail.hu>
2021-09-17 18:20:17 +0200

    I defined menuSelectedInput method of Verification class

---
+++ Aviation_project/Aviation_project/Verification.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <regex>
+#include "Verification.h"
+
+using namespace std;
+
+int Verification::menuSelectedInput()
+{
+    const regex pattern("[a-zA-Z<>#§&@{}:;,?+!%/=()-_*$ß€đĐ]*");
+    string selected;
+    cin >> selected;
+
+    if (regex_match(selected, pattern)) return -1;
+
+    return stoi(selected);
+}
